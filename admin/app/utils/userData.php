528a14e48587b2bc69a279ccd63a30b390e32f46
Victor Reyes Galvez <tenshi98@gmail.com>
2026-02-09 16:48:43 -0300

    Se alinean las lineas de codigo
    Se alinean las lineas de codigo

--- admin/app/utils/userData.php
+++ admin/app/utils/userData.php
@@ -2,12 +2,12 @@
 /*******************************************************************************************************************/
 /*                                            Pantalla de inicio                                                   */
 /*******************************************************************************************************************/
-$f3->route('GET /principal', 'main->principal');                                                      //pantalla principal
-$f3->route('GET /principal/resumenCampana/@id', 'mainCampanas->resumenCampana');                      //Resumen Campañas
-$f3->route('GET /principal/campanaAnalisisContable/@id', 'mainCampanas->campanaAnalisisContable');    //Analisis Contable
-$f3->route('GET /principal/partidaConfirmada', 'mainCampanas->partidaConfirmada');                    //Últimas partidas confirmadas
-$f3->route('GET /principal/docMercantiles', 'mainDocMercantiles->pagosPendientes');                   //Pagos pendientes de los documentos mercantiles
-$f3->route('GET /principal/bodegaStock', 'mainBodegas->stocksProductos');                             //Productos con bajo Stock en las bodegas
+$f3->route('GET /principal',                             'main->principal');                                  //pantalla principal
+$f3->route('GET /principal/resumenCampana/@id',          'gestionCampanasWidgets->resumenCampana');           //Resumen Campañas
+$f3->route('GET /principal/campanaAnalisisContable/@id', 'gestionCampanasWidgets->campanaAnalisisContable');  //Analisis Contable
+$f3->route('GET /principal/partidaConfirmada',           'gestionCampanasWidgets->partidaConfirmada');        //Últimas partidas confirmadas
+$f3->route('GET /principal/docMercantiles',              'gestionDocumentosWidgets->pagosPendientes');        //Pagos pendientes de los documentos mercantiles
+$f3->route('GET /principal/bodegaStock',                 'bodegasWidgets->stocksProductos');                  //Productos con bajo Stock en las bodegas
 
 /*******************************************************************************************************************/
 /*                                                 Mi Usuario                                                      */
@@ -18,8 +18,8 @@ $f3->route('GET /perfil', 'miUsuario->view');                   //Ver mi perfil
 $f3->route('GET /perfil/UpdateData', 'miUsuario->FRG_UpdateData');  //Actualizar datos del perfil
 $f3->route('GET /perfil/UpdateCard', 'miUsuario->FRG_UpdateCard');  //Actualizar imagen del perfil
 //Acciones
-$f3->route('GET /auth/logout', 'miUsuario->logout');        //Cerrar la sesion
-$f3->route('POST /perfil/update', 'miUsuario->update');     //Editar por post (modificar y subir archivos)
+$f3->route('GET /auth/logout',     'miUsuario->logout');    //Cerrar la sesion
+$f3->route('POST /perfil/update',  'miUsuario->update');    //Editar por post (modificar y subir archivos)
 $f3->route('PUT /perfil/delFiles', 'miUsuario->delFiles');  //Permite eliminar el archivo de la imagen
 
 /*******************************************************************************************************************/
