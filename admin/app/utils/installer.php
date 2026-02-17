6164f7cdeea24270b2d16fb73fc0974b1c6d5a71
Victor Reyes Galvez <tenshi98@gmail.com>
2026-02-17 18:16:49 -0300

    Actualizacion rutas instalador
    Actualizacion rutas instalador

--- admin/app/utils/installer.php
+++ admin/app/utils/installer.php
@@ -8,9 +8,15 @@ $f3->route('POST /Api/auth/logout', 'apiSession->Apilogout');    //POST - logout
 /*                                              Rutas y acceso a las APIS                                          */
 /*******************************************************************************************************************/
 //Rutas
-$f3->route('GET /install',              'installer->welcome');     //Paso 1: Bienvenida
-$f3->route('POST /install/credentials', 'installer->credentials'); //Paso 2: Credenciales MySQL
-$f3->route('POST /install/database',    'installer->database');    //Paso 3: Configuración de Base de Datos
-$f3->route('POST /install/summary',     'installer->summary');     //Paso 4: Resumen
-$f3->route('POST /install/finish',      'installer->finish');      //Paso 5: Finalización
+$f3->route('GET /install',             'installer->welcome');     //Paso 1: Bienvenida
+$f3->route('GET /install/credentials', 'installer->credentials'); //Paso 2: Credenciales MySQL
+$f3->route('GET /install/database',    'installer->database');    //Paso 3: Configuración de Base de Datos
+$f3->route('GET /install/summary',     'installer->summary');     //Paso 4: Resumen
+$f3->route('GET /install/finish',      'installer->finish');      //Paso 5: Finalización
+
+
+$f3->route('POST /install/credentials', 'installer->credentialsPost'); //Paso 2: Credenciales MySQL
+$f3->route('POST /install/database',    'installer->databasePost');    //Paso 3: Configuración de Base de Datos
+$f3->route('POST /install/summary',     'installer->summaryPost');     //Paso 4: Resumen
+$f3->route('POST /install/finish',      'installer->finishPost');      //Paso 5: Finalización
 
