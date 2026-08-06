f594f47ab47015fe84eee2b99355965359fdcd83
Victor Reyes Galvez <54037366+tenshi98@users.noreply.github.com>
2026-08-06 11:55:30 -0400

    Se agrega nueva regla
    Se agrega nueva regla

--- vendors/application/utils/Response.php
+++ vendors/application/utils/Response.php
@@ -56,6 +56,7 @@ class Response {
         header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
         header('Access-Control-Allow-Headers: Content-Type, Authorization');
         header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-*');
 
         // Estructura estándar de respuesta
         $response = [
