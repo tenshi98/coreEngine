6cf68a32547c5045692d28431976ed32163bd965
Victor Reyes Galvez <54037366+tenshi98@users.noreply.github.com>
2026-08-30 14:09:42 -0400

    Se cambia validacion
    Se cambia validacion

--- admin/app/utils/loadErrors.php
+++ admin/app/utils/loadErrors.php
@@ -3,9 +3,6 @@
 /*                                                     Manejo de errores                                           */
 /*******************************************************************************************************************/
 
-//verifica la capa de desarrollo
-$whitelist = ['localhost', '127.0.0.1', '::1', '172.18.0.1'];
-$isDev     = in_array($_SERVER['REMOTE_ADDR'], $whitelist, true);
 //En el caso de no estar en desarrollo ni ser superadministrador
 if (!$isDev && $f3->get('SESSION.DataInfo.UserType') != 1) {
     // Página de error y manejo de rutas inexistentes
