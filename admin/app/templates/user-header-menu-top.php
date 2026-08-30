ccaac4f1e1c5ef3474f0a6f7f967ec746af248c5
Victor Reyes Galvez <54037366+tenshi98@users.noreply.github.com>
2026-08-30 14:09:27 -0400

    Actualizacion del menu
    Actualizacion del menu

--- admin/app/templates/user-header-menu-top.php
+++ admin/app/templates/user-header-menu-top.php
@@ -5,13 +5,13 @@
     <?php
     //Verifico si hay datos
     if(is_array($_SESSION['arrMenu'])){
-        //Variables iniciales
+        // Variables iniciales
         $Colmn      = 9;
         $arrMenu    = '<li class="col-xs-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4">';
         $countMenu  = 0;
         //Recorro
         foreach ($_SESSION['arrMenu'] as $Categoria=>$permisos){
-            //Variables
+            // Variables
             $countMenu = 0;
             $arrMenu .= '<ul><li class="dropdown-tittle">'.$Categoria.'</li>';
             //se recorren los datos dentro de la categoría
